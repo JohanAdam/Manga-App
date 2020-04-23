af9cd152513d6c629d81c325edf250e7007048e2
JohanAdam <johan@thelorry.com>
2020-04-23 23:46:21 +0800

    Add interceptor

--- app/src/main/java/com/example/android/marsrealestate/network/ApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/ApiService.kt
@@ -21,7 +21,8 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
-import retrofit2.Call
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -43,11 +44,23 @@ private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
+private fun getClient(): OkHttpClient {
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.apply {
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+    }
+    return  OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+}
+
+
 //Retrofit builder.
 private val retrofit = Retrofit.Builder()
         //For add coroutines support.
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(getClient())
         .baseUrl(BASE_URL)
         .build()
 
